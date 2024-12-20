f26a85e7f73cfa00694e457feabaa1be74f3520d
Ahoudori77 <rakurusu77@gmail.com>
2024-12-20 19:58:49 +0900

    Implement usage record creation and inventory update

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :inventory, only: [:index, :update]
+      resources :usage_records, only: [:create]
     end
   end
 end
