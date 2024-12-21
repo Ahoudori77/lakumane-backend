ccddcae0b3a5c26533145f7f729cf74a6040276b
Ahoudori77 <rakurusu77@gmail.com>
2024-12-21 11:21:15 +0900

    WebSocket対応の通知更新機能を実装

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,17 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  after_create_commit { broadcast_notification }
+
+  private
+
+  def broadcast_notification
+    NotificationsChannel.broadcast_to(
+      user,
+      message: message,
+      read: read,
+      created_at: created_at
+    )
+  end
 end
